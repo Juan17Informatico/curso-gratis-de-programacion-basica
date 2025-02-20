77114712cbf4357f0d1c5d6f00e8d6362879907f
JuanCM <juancampuzano2356@gmail.com>
2025-02-20 09:59:19 -0500

    Clase 22 Finalizada

--- 01-fundamentos-de-programacion/mokepon/js/logica.js
+++ 01-fundamentos-de-programacion/mokepon/js/logica.js
@@ -6,21 +6,20 @@ function iniciarJuego (){
 const selectPlayerPet = () => {
     // Selecciona todos los elementos con la clase "pets" (radio buttons)
     const pets = document.querySelectorAll('.pets');
-    console.log({ pets }); // Muestra en la consola el NodeList de mascotas encontradas
+    
+    const spanMascotaJugador = document.getElementById('pet-player');
 
-    /**
-     * Convierte el NodeList en un array y usa el método `.some()` para recorrer los elementos.
-     * `.some()` detiene la iteración cuando encuentra un elemento que cumple la condición.
-     * Si algún elemento tiene `checked` en `true`, muestra una alerta con su nombre formateado.
-     */
-    const selected = [...pets].some(element => {
+    const selected = [...pets].find(element => {
         if (element.checked) { // Verifica si el elemento está seleccionado
             const charToUpperCase = element.value.charAt(0).toUpperCase(); // Convierte la primera letra en mayúscula
-            alert(`Seleccionaste a: ${charToUpperCase + element.value.slice(1)}`); // Muestra el mensaje con el nombre formateado
-            return true; // Retorna `true` para detener la iteración
+            const formattedText = charToUpperCase + element.value.slice(1);
+            spanMascotaJugador.innerHTML = formattedText;
+            return element; // Retorna `true` para detener la iteración
         }
         return false; // Continúa iterando si el elemento no está seleccionado
     });
+
+    console.log({selected});
 
     // Si ninguna mascota fue seleccionada, muestra una alerta al usuario
     if (!selected) {
