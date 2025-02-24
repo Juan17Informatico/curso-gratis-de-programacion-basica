9c69a53a2f44fb1fcdce81596b6019e1beeb89b9
JuanCM <juancampuzano2356@gmail.com>
2025-02-24 11:22:09 -0500

    Clase 23 Finalizada

--- 01-fundamentos-de-programacion/mokepon/js/logica.js
+++ 01-fundamentos-de-programacion/mokepon/js/logica.js
@@ -3,17 +3,21 @@ function iniciarJuego (){
     btnPetPlayer.addEventListener('click', selectPlayerPet);
 }
 
-const selectPlayerPet = () => {
+function random(min,max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function selectPlayerPet() {
     // Selecciona todos los elementos con la clase "pets" (radio buttons)
     const pets = document.querySelectorAll('.pets');
     
-    const spanMascotaJugador = document.getElementById('pet-player');
+    const spanPlayerPet = document.getElementById('pet-player');
 
     const selected = [...pets].find(element => {
         if (element.checked) { // Verifica si el elemento está seleccionado
             const charToUpperCase = element.value.charAt(0).toUpperCase(); // Convierte la primera letra en mayúscula
             const formattedText = charToUpperCase + element.value.slice(1);
-            spanMascotaJugador.innerHTML = formattedText;
+            spanPlayerPet.innerHTML = formattedText;
             return element; // Retorna `true` para detener la iteración
         }
         return false; // Continúa iterando si el elemento no está seleccionado
@@ -25,7 +29,24 @@ const selectPlayerPet = () => {
     if (!selected) {
         alert('No seleccionaste ninguna mascota!');
     }
+
+    selectEnemyPet();
 };
+
+function selectEnemyPet(){
+
+    const pets = document.querySelectorAll('.pets');
+    const petsArray = [...pets]
+    const randomAttack = random( 0, petsArray.length - 1);
+    const spanEnemyPet = document.getElementById('pet-enemy');
+
+    if(petsArray[randomAttack]){
+        const charToUpperCase = petsArray[randomAttack].value.charAt(0).toUpperCase(); // Convierte la primera letra en mayúscula
+        const formattedText = charToUpperCase + petsArray[randomAttack].value.slice(1);
+        spanEnemyPet.innerHTML = formattedText;
+    }
+
+}
 
 
 window.addEventListener('load', iniciarJuego);
